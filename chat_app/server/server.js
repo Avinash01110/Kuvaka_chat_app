77bec69324f9b15d341269cf6062c0374f501e2a
Avinash01110 <gavinash321098@gmail.com>
2024-11-27 18:03:05 +0530

    server file updated

--- chat_app/server/server.js
+++ chat_app/server/server.js
@@ -1,3 +1,17 @@
-const { startServer } = require('./controllers/chatController');
+const net = require('net');
+const { log } = require('./utils/logger');
 
-startServer();
+
+const PORT = process.env.PORT || 3000;
+
+const handleNewConnection = (socket) => {
+  log('A new client connected');
+  socket.write('Welcome to the chat server!\n');
+};
+
+const server = net.createServer(handleNewConnection);
+
+
+server.listen(PORT, '0.0.0.0', () => {
+  log(`Server is running on port ${PORT}`);
+});
